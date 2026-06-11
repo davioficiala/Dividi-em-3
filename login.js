58938462109f699de18a748cb6a1f8caff3b7fe2
davioficiala <daviwalker813@gmail.com>
2026-06-11 14:46:09 -0300

    Update login.js

--- login.js
+++ login.js
@@ -13,4 +13,43 @@ const firebaseConfig = {
   authDomain: "deus-fiel-7a2cc.firebaseapp.com",
   projectId: "deus-fiel-7a2cc",
   storageBucket: "deus-fiel-7a2cc.firebasestorage.app",
-  messagingSenderId: "
+  messagingSenderId: "746071893459",
+  appId: "1:746071893459:web:c7a31b6e8ad49709187b26"
+};
+
+// Iniciar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Auth
+const auth = getAuth(app);
+
+document.getElementById("btnLogin").addEventListener("click", async () => {
+
+  try {
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    await signInWithEmailAndPassword(
+      auth,
+      email,
+      senha
+    );
+
+    document.getElementById("statusAuth").innerText =
+      "✅ Login realizado";
+
+    setTimeout(() => {
+      window.location.href = "menu.html";
+    }, 1000);
+
+  } catch (erro) {
+
+    console.error(erro);
+
+    document.getElementById("statusAuth").innerText =
+      erro.message;
+
+  }
+
+});
