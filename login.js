91b66c6487d02646b7da7c6b16ba2b6af6271902
davioficiala <daviwalker813@gmail.com>
2026-06-11 13:47:10 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,16 @@
+// Firebase App
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+// Firebase Auth
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// Configuração Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAdirRN9nHaekNUjM3upyPTSBRVollrlMI",
+  authDomain: "deus-fiel-7a2cc.firebaseapp.com",
+  projectId: "deus-fiel-7a2cc",
+  storageBucket: "deus-fiel-7a2cc.firebasestorage.app",
+  messagingSenderId: "
